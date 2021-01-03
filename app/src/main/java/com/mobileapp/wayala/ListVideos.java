ee34d6439df96f8a283a13d2bd926fd9d0a523a3
paulo-dot-matos <pauloromatos@outlook.pt>
2021-01-03 18:54:45 +0000

    prototype working

---
+++ app/src/main/java/com/mobileapp/wayala/ListVideos.java
@@ -0,0 +1,14 @@
+package com.mobileapp.wayala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListVideos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_videos);
+    }
+}
